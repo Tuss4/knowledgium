b65466e11596ec8cb88eb51f1785858e6db59869
tuss4 <tuss4dbfn@gmail.com>
2014-09-21 21:07:32 -0500

    Updates to Vagrantfile, added docker, getting ready to add fig.

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,4 +8,7 @@ apt-get install -y python-dev
 apt-get install -y libpq-dev
 apt-get install -y postgresql
 apt-get install -y vim
+apt-get install -y docker.io
+ln -sf /usr/bin/docker.io /usr/local/bin/docker
+sed -i '$acomplete -F _docker docker' /etc/bash_completion.d/docker.io
 sudo pip install fabric
